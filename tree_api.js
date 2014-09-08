6f706bb32de08fc476ec89acb3db139fa08ce49c
drda3x <da3x11@gmail.com>
2014-09-09 01:51:03 +0400

    added sort of nodes

--- tree_api.js
+++ tree_api.js
@@ -1,114 +1,123 @@
-(function() {
-	'use strict';
+(function () {
+    'use strict';
 
-	/**
-	 * Class for create and manipulate the tree structure
-	 */
-	function TreeData(id, name) {
-		this.id = id;
-		this.name = name;
-		this.content = [];
-		this.anotherRelations = []
-	}
+    /**
+     * Class for create and manipulate the tree structure
+     */
+    function TreeData(id, name) {
+        this.id = id;
+        this.name = name;
+        this.content = [];
+        this.anotherRelations = []
+    }
 
-	/**
-	 * Method to add data-content of the node
-	 */
-	TreeData.prototype.add = function(object) {
-		if (object instanceof TreeData) {
-			this.content.push(object);
-		} else {
-			if (!object.name || !(object.name instanceof 'string')) {
-				throw 'TreeData -> add()... insorrect name of the incoming object...';
-			}
-			if (!object.id || !(object.id instanceof 'number')) {
-				throw 'TreeData -> add()... incorrect id of the incoming object...';
-			}
-			_object = new TreeData(object.id, object.name);
-			this.content.push(_object);
-		}
+    /**
+     * Method to add data-content of the node
+     */
+    TreeData.prototype.add = function (object) {
+        if (object instanceof TreeData) {
+            this.content.push(object);
+        } else {
+            if (!object.name || !(object.name instanceof 'string')) {
+                throw 'TreeData -> add()... insorrect name of the incoming object...';
+            }
+            if (!object.id || !(object.id instanceof 'number')) {
+                throw 'TreeData -> add()... incorrect id of the incoming object...';
+            }
+            var _object = new TreeData(object.id, object.name);
+            this.content.push(_object);
+        }
 
-		return this;
-	}
+        return this;
+    }
 
-	TreeData.prototype.find = function(id) {
-		if (!(id instanceof 'number')) {
-			throw 'TreeData -> find.. ID should be a number';
-		}
-		if (this.id == id) {
-			return this;
-		} else {
-			var fiinded;
-			for (var i= 0, j= this.content.length; i<j; i++) {
-				finded = this.content[i].find(id);
-			}
-			return finded;
-		}
-	}
+    TreeData.prototype.find = function (id) {
+        if (!(id instanceof 'number')) {
+            throw 'TreeData -> find.. ID should be a number';
+        }
+        if (this.id == id) {
+            return this;
+        } else {
+            var finded;
+            for (var i = 0, j = this.content.length; i < j; i++) {
+                finded = this.content[i].find(id);
+            }
+            return finded;
+        }
+    }
 
-	var test = [
-		{
-			id: 0,
-			name: 'core-node',
-			relations: []
-		},
-		{
-			id: 1,
-			name: 'level-1',
-			relations: [0]
-		},
-		{
-			id: 2,
-			name: 'level-2',
-			relations: [1]
-		},
-		{
-			id: 3,
-			name: 'level-2',
-			relations: [1]
-		},
-		{
-			id: 4,
-			name: 'level-3',
-			relations: [2,3]
-		}
-	]
+    var nodes = [
+        {
+            id: 4,
+            name: 'level-3',
+            relations: {
+                parent: [2,3]
+            }
+        },
+        {
+            id: 1,
+            name: 'level-1',
+            relations: {
+                parent: 0
+            }
+        },
+        {
+            id: 0,
+            name: 'core-node',
+            relations: {
+                parent: null
+            }
+        },
+        {
+            id: 3,
+            name: 'level-2',
+            relations: {
+                parent: 0,
+                neighbors: 2
+            }
+        },
+        {
+            id: 2,
+            name: 'level-2',
+            relations: {
+                parent: 0,
+                neighbors: 3
+            }
+        }
+    ];
 
-	// find core level
+    nodes = nodes.sort(function(a,b) {
+        var a = a.relations.parent,
+            b = b.relations.parent;
 
-	var tree;
-	for(var i= 0, j= test.length; i<j; i++) {
-		if (test.relations.length == 0) {
-			tree = new TreeData(test[i].id, test[i].name);
-		}
-	}
+        if(a instanceof Array){
+            a.sort();
+            a = a[0];
+        }
 
-	for (var i= 1, j= test.length; i<j; i++) {
-		var self = tree.find(test[i].id);
-		if(!self) {
-			// Если не нашел
-			var parent;
-			self = new TreeData(test[i].id, test[i].name);
-			for(var i1= 0, j1= test[i].relations.length; i1<j1; i1++) {
-				if(!parent) {
-					parent = tree.find(test[i].relations[i1].id)
-					parent.add(self);
-				} else {
-					self.anotherRelations.push(test[i].relations[i1]);
-				}
-			}
-		} else {
-			// Если нашел
-		}
-	}
+        if(b instanceof Array) {
+            b.sort();
+            b = b[0];
+        }
 
-})()
+        if(a == null) {
+            return -1;
+        } else if (b == null) {
+            return +1;
+        } else {
+            return a - b;
+        }
+    });
+
+    console.log(nodes);
+
+})();
 
 
 /*
-	Для каждого объекта нужно:
-	1. Проверить есть ли в дереве родитель
-	2. Если есть - добавить объект в родительский список связи
-	3. Если нет - добавить родителя в дерево, а потом и сам объект
-	4. И т.д.
-*/
+ Для каждого объекта нужно:
+ 1. Проверить есть ли в дереве родитель
+ 2. Если есть - добавить объект в родительский список связи
+ 3. Если нет - добавить родителя в дерево, а потом и сам объект
+ 4. И т.д.
+ */
