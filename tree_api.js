67f122b7fc32961a720ba2ad09f816fa1d98ec66
Vasily Nesterov <da3x11@gmail.com>
2014-09-09 13:33:14 +0400

    Добавлен поиск слоя

--- tree_api.js
+++ tree_api.js
@@ -1,117 +1,68 @@
-(function () {
+(function (g) {
     'use strict';
 
     /**
      * Class for create and manipulate the tree structure
      */
-    function TreeData(id, name) {
-        this.id = id;
-        this.name = name;
-        this.content = [];
-        this.anotherRelations = []
+    function Tree(content) {
+        this.structure = [];
     }
 
     /**
      * Method to add data-content of the node
      */
-    TreeData.prototype.add = function (object) {
-        if (object instanceof TreeData) {
+    Tree.prototype.add = function (object) {
+        if (object instanceof TreeElement) {
             this.content.push(object);
         } else {
-            if (!object.name || !(object.name instanceof 'string')) {
-                throw 'TreeData -> add()... insorrect name of the incoming object...';
+            throw 'Tree.add() -> object has to be an instance of TreeElement'
             }
-            if (!object.id || !(object.id instanceof 'number')) {
-                throw 'TreeData -> add()... incorrect id of the incoming object...';
-            }
-            var _object = new TreeData(object.id, object.name);
-            this.content.push(_object);
         }
 
         return this;
     }
 
-    TreeData.prototype.find = function (id) {
+    Tree.prototype.find = function (id) {
         if (!(id instanceof 'number')) {
-            throw 'TreeData -> find.. ID should be a number';
-        }
-        if (this.id == id) {
-            return this;
+            throw 'Tree.find() ID should be a number';
         } else {
-            var finded;
-            for (var i = 0, j = this.content.length; i < j; i++) {
-                finded = this.content[i].find(id);
-            }
-            return finded;
+        	for(level in this.structure) {
+        		for(element in level) {
+        			if(element.id == id) {
+        				return element;
+        			}
+        		}
+        	}
+        	return null;
         }
     }
 
-    var nodes = [
-        {
-            id: 4,
-            name: 'level-3',
-            relations: {
-                parent: [2,3]
-            }
-        },
-        {
-            id: 1,
-            name: 'level-1',
-            relations: {
-                parent: 0
-            }
-        },
-        {
-            id: 0,
-            name: 'core-node',
-            relations: {
-                parent: null
-            }
-        },
-        {
-            id: 3,
-            name: 'level-2',
-            relations: {
-                parent: 0,
-                neighbors: 2
-            }
-        },
-        {
-            id: 2,
-            name: 'level-2',
-            relations: {
-                parent: 0,
-                neighbors: 3
-            }
-        }
-    ];
+    Tree.prototype.draw = function() {
 
-    nodes = nodes.sort(function(a,b) {
-        var a = a.relations.parent,
-            b = b.relations.parent;
+    }
 
-        if(a instanceof Array){
-            a.sort();
-            a = a[0];
-        }
+    function TreeElement(id, name, parent, neighbors) {
+    	this.id = id;
+    	this.name = name;
+    	this.parent = parent;
+    	this.neighbors = neighbors;
+    }
 
-        if(b instanceof Array) {
-            b.sort();
-            b = b[0];
-        }
+    if(!q){
+    	return {
+    		TreeElement: TreeElement,
+    		Tree: Tree
+    	}
+    }
 
-        if(a == null) {
-            return -1;
-        } else if (b == null) {
-            return +1;
-        } else {
-            return a - b;
-        }
-    });
+    if(!g.ics) {
+    	g.ics = {}
+    }
 
-    console.log(nodes);
+    g.ics.TreeElement = TreeElement;
+    g.ics.Tree = Tree;
 
-})();
+})(this);
 
 
 /*
